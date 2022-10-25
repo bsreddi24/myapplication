9952d22c40cdd24efca12ad35167352eb1065afb
santosh <bsreddi24@gmail.com>
2022-10-25 11:59:31 +0100

    Added Second and Third Activites and Xml files respectively

---
+++ app/src/main/java/com/w9577326/myapplication/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.w9577326.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
