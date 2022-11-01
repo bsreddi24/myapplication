35374ab513ddf76f731604547c474515e4bbe203
santosh <bsreddi24@gmail.com>
2022-11-01 19:36:07 +0000

    Created Login Page with username and password fields and added social login icons also

--- app/src/main/java/com/w9577326/myapplication/MainActivity.java
+++ app/src/main/java/com/w9577326/myapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.w9577326.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,4 +24,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent (this, SecondActivity.class);
         startActivity(intent);
     }
+
 }
