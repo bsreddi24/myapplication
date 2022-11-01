9f23ace5821e255e851d3983aa2bf1a78c3782d2
w9577326 <bsreddi24@gmail.com>
2022-11-01 11:10:27 +0000

    Added Onclick property for the buttons

--- app/src/main/java/com/w9577326/myapplication/SecondActivity.java
+++ app/src/main/java/com/w9577326/myapplication/SecondActivity.java
@@ -2,7 +2,9 @@ package com.w9577326.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+    public void goToActivity2 (View view){
+//        Intent intent = new Intent (this, SecondActivity.class);
+//      startActivity(intent);
+        finish();
     }
 }
