b0bcc8f491e11e33fbdad06dabefe3567549a2a9
santosh <bsreddi24@gmail.com>
2022-12-09 14:40:16 +0000

    Expenses Fragment is modified to retreview data from the expenses Firebase firestore documents

---
+++ app/src/main/java/com/w9577326/myapplication/AdapterExpenses.java
@@ -0,0 +1,51 @@
+package com.w9577326.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+public class AdapterExpenses extends FirestoreRecyclerAdapter<Expense, AdapterExpenses.ExpensesViewHolder>{
+    ExpensesFragment context;
+
+    public AdapterExpenses(@NonNull FirestoreRecyclerOptions<Expense> options, ExpensesFragment context) {
+        super(options);
+        this.context = context;
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull ExpensesViewHolder holder, int position, @NonNull Expense expense) {
+        holder.expenses_amount_v.setText(expense.amount);
+        holder.expenses_details_v.setText(expense.details);
+//        holder.expenses_timestamp_v.setText(expense.timestamp);
+
+    }
+
+    @NonNull
+    @Override
+    public ExpensesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_expenses,parent,false);
+        return new ExpensesViewHolder(view);
+    }
+
+
+    class ExpensesViewHolder extends RecyclerView.ViewHolder{
+
+        TextView expenses_amount_v, expenses_details_v, expenses_timestamp_v;
+
+        public ExpensesViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            expenses_amount_v = itemView.findViewById(R.id.expenses_amount_v);
+            expenses_details_v = itemView.findViewById(R.id.expenses_details_v);
+            expenses_timestamp_v = itemView.findViewById(R.id.expenses_timestamp_v);
+        }
+    }
+}
--- app/src/main/java/com/w9577326/myapplication/ExpensesFragment.java
+++ app/src/main/java/com/w9577326/myapplication/ExpensesFragment.java
@@ -3,13 +3,20 @@ package com.w9577326.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.Query;
 
 ///**
 // * A simple {@link Fragment} subclass.
@@ -18,6 +25,10 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 // */
 public class ExpensesFragment extends Fragment {
     FloatingActionButton add_btnExp;
+    //    RecyclerView recyclerIncome;
+    AdapterExpenses adapterExpenses;
+    RecyclerView recyclerView;
+    ImageButton menuBtn;
 
 //    // TODO: Rename parameter arguments, choose names that match
 //    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -63,9 +74,18 @@ public class ExpensesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v =  inflater.inflate(R.layout.fragment_expenses, container, false);
+        View r =  inflater.inflate(R.layout.fragment_expenses, container, false);
+        return r;
 
-        add_btnExp = v.findViewById(R.id.add_btnExp);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setupRecyclerView(view);
+        add_btnExp = view.findViewById(R.id.add_btnExp);
+        View recyclerExpenses = view.findViewById(R.id.recycler_expenses);
+
         add_btnExp.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -73,6 +93,39 @@ public class ExpensesFragment extends Fragment {
                 startActivity(intent);
             }
         });
-        return v;
+    }
+
+    private void setupRecyclerView(View r){
+        recyclerView = r.findViewById(R.id.recycler_expenses);
+        menuBtn = r.findViewById(R.id.menu_btn);
+        menuBtn.setOnClickListener((v)->showMenu());
+        Query query = Utility.getCollectionReferenceForExpenses().orderBy("amount", Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<Expense> options = new FirestoreRecyclerOptions.Builder<Expense>()
+                .setQuery(query,Expense.class).build();
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+
+        adapterExpenses = new AdapterExpenses(options, this);
+        recyclerView.setAdapter(adapterExpenses);
+    }
+    void showMenu(){
+        // TODO Display Menu
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapterExpenses.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapterExpenses.stopListening();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        adapterExpenses.notifyDataSetChanged();
     }
 }
